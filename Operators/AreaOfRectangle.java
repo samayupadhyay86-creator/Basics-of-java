77a826391104bb40f78dfde14980f69c1f8afdbf
samay upadhyay <samayupadhyay86@gmail.com>
2026-08-12 07:44:23 +0530

    Added Succesfully

---
+++ Operators/AreaOfRectangle.java
@@ -0,0 +1,11 @@
+package Operators;
+
+// Write a program to find a Area Of Rectangles.
+public class AreaOfRectangle {
+    public static void main(String[] args) {
+        int l = 10;
+        int b = 20;
+        int areaofrectangle = (l*b);
+        System.out.println(areaofrectangle);
+    }
+}
